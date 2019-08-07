93a419bc65107e93a5a385b17433467c767cdfcc
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-07 11:51:33 -0400

    refactor using context

---
+++ src/Context.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import dummyStore from './dummy-store';
+
+const Context = React.createContext({
+  folders: dummyStore.folders,
+  notes: dummyStore.notes
+});
+
+export default Context;
--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CircleButton from '../CircleButton/CircleButton'
-import './NotePageNav.css'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CircleButton from '../CircleButton/CircleButton';
+import Context from '../Context';
+import { findNote, findFolder } from '../notes-helpers';
+import './NotePageNav.css';
 
-export default function NotePageNav(props) {
-  return (
-    <div className='NotePageNav'>
-      <CircleButton
-        tag='button'
-        role='link'
-        onClick={() => props.history.goBack()}
-        className='NotePageNav__back-button'
-      >
-        <FontAwesomeIcon icon='chevron-left' />
-        <br />
-        Back
-      </CircleButton>
-      {props.folder && (
-        <h3 className='NotePageNav__folder-name'>
-          {props.folder.name}
-        </h3>
-      )}
-    </div>
-  )
+class NotePageNav extends React.Component {
+  static contextType = Context;
+  render() {
+    const { folders, notes } = this.context;
+    const { noteId } = this.props.match.params;
+    const note = findNote(notes, noteId) || {};
+    const folder = findFolder(folders, note.folderId);
+    return (
+      <div className="NotePageNav">
+        <CircleButton
+          tag="button"
+          role="link"
+          onClick={() => this.props.history.goBack()}
+          className="NotePageNav__back-button"
+        >
+          <FontAwesomeIcon icon="chevron-left" />
+          <br />
+          Back
+        </CircleButton>
+        {folder && <h3 className="NotePageNav__folder-name">{folder.name}</h3>}
+      </div>
+    );
+  }
 }
 
 NotePageNav.defaultProps = {
   history: {
     goBack: () => {}
   }
-}
+};
+
+export default NotePageNav;
